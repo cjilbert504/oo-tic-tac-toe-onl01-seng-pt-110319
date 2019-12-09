e38147d63501851e5f05440a8bdeccef0848680a
Collin Jilbert <cjilbert504@gmail.com>
2019-12-09 01:14:22 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,30 @@
+class TicTacToe
+
+attr_accessor :board
+  def initialize
+    @board = Array.new(9, " ")
+  end
+
+  WIN_COMBINATIONS = [
+    [0,1,2], #Top row
+    [3,4,5], #Middle row
+    [6,7,8], #Bottom row
+    [0,3,6], #Left vertical
+    [1,4,7], #middle vertical
+    [2,5,8], #right vertical
+    [0,4,8], #diagonal from left to right
+    [2,4,6]  #diagonal from right to left
+  ]
+
+  def display_board
+   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+   puts "-----------"
+   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+   puts "-----------"
+   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+end
